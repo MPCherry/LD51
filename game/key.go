5f449298815a4802b029432cfb0994d4380fad7e
Matthew <mpinchuckcherry@gmail.com>
2022-10-01 20:43:38 +0100

    Noisify ghosts

--- game/key.go
+++ game/key.go
@@ -7,15 +7,17 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-var spriteKey, _, _ = ebitenutil.NewImageFromFile("resources/pink.png")
+var spriteKey, _, _ = ebitenutil.NewImageFromFile("resources/sprites/key.png")
 
 type Key struct {
-	x           float64
-	originX     float64
-	y           float64
-	originY     float64
-	active      bool
-	spriteIndex int
+	x            float64
+	originX      float64
+	y            float64
+	originY      float64
+	active       bool
+	spriteIndex  int
+	frame        int
+	frameCounter int
 }
 
 func (k *Key) X() float64 {
@@ -27,7 +29,7 @@ func (k *Key) Y() float64 {
 }
 
 func (k *Key) Image() *ebiten.Image {
-	return spriteKey.SubImage(image.Rect(16*k.spriteIndex, 0, 16*k.spriteIndex+16, 16)).(*ebiten.Image)
+	return spriteKey.SubImage(image.Rect(16*k.spriteIndex, 16*k.frame, 16+16*k.spriteIndex, 16+16*k.frame)).(*ebiten.Image)
 }
 
 func (k *Key) Active() bool {
@@ -35,4 +37,12 @@ func (k *Key) Active() bool {
 }
 
 func (k *Key) Update(world *World, inputs []ebiten.Key) {
+	k.frameCounter++
+	if k.frameCounter == 10 {
+		k.frame++
+		k.frameCounter = 0
+	}
+	if k.frame == 4 {
+		k.frame = 0
+	}
 }
