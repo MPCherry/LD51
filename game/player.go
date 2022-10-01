4010c09543a14b3fbb994895d83ae5d990648b84
Matthew <mpinchuckcherry@gmail.com>
2022-10-01 19:13:56 +0100

    Add player sprites

--- game/player.go
+++ game/player.go
@@ -9,7 +9,14 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-var spritePlayer, _, _ = ebitenutil.NewImageFromFile("resources/small_black_square.png")
+var spritePlayer, _, _ = ebitenutil.NewImageFromFile("resources/sprites/player3.png")
+var spritePlayerLeft, _, _ = ebitenutil.NewImageFromFile("resources/sprites/player3left.png")
+var spritePlayerJump, _, _ = ebitenutil.NewImageFromFile("resources/sprites/playerjump.png")
+var spritePlayerJumpLeft, _, _ = ebitenutil.NewImageFromFile("resources/sprites/playerjumpleft.png")
+var spriteGhost, _, _ = ebitenutil.NewImageFromFile("resources/sprites/ghost3.png")
+var spriteGhostLeft, _, _ = ebitenutil.NewImageFromFile("resources/sprites/ghost3left.png")
+var spriteGhostJump, _, _ = ebitenutil.NewImageFromFile("resources/sprites/ghostjump.png")
+var spriteGhostJumpLeft, _, _ = ebitenutil.NewImageFromFile("resources/sprites/ghostjumpleft.png")
 var keyRecording = [][]ebiten.Key{}
 
 const (
@@ -30,6 +37,9 @@ type Player struct {
 	active        bool
 	carrying      *Key
 	letGoOfPickup bool
+	frame         int
+	frameCounter  int
+	facing        bool
 }
 
 func (p *Player) X() float64 {
@@ -42,9 +52,27 @@ func (p *Player) Y() float64 {
 
 func (p *Player) Image() *ebiten.Image {
 	if p.first {
-		return spritePlayer.SubImage(image.Rect(0, 0, 16, 16)).(*ebiten.Image)
+		if p.facing {
+			if p.jumped {
+				return spritePlayerJumpLeft.SubImage(image.Rect(16, 0, 0, 16)).(*ebiten.Image)
+			}
+			return spritePlayerLeft.SubImage(image.Rect(16, 16*p.frame, 0, 16+16*p.frame)).(*ebiten.Image)
+		}
+		if p.jumped {
+			return spritePlayerJump.SubImage(image.Rect(16, 0, 0, 16)).(*ebiten.Image)
+		}
+		return spritePlayer.SubImage(image.Rect(16, 16*p.frame, 0, 16+16*p.frame)).(*ebiten.Image)
 	} else {
-		return spritePlayer.SubImage(image.Rect(16, 0, 32, 16)).(*ebiten.Image)
+		if p.facing {
+			if p.jumped {
+				return spriteGhostJumpLeft.SubImage(image.Rect(16, 0, 0, 16)).(*ebiten.Image)
+			}
+			return spriteGhostLeft.SubImage(image.Rect(16, 16*p.frame, 0, 16+16*p.frame)).(*ebiten.Image)
+		}
+		if p.jumped {
+			return spriteGhostJump.SubImage(image.Rect(16, 0, 0, 16)).(*ebiten.Image)
+		}
+		return spriteGhost.SubImage(image.Rect(16, 16*p.frame, 0, 16+16*p.frame)).(*ebiten.Image)
 	}
 }
 
@@ -53,6 +81,14 @@ func (p *Player) Active() bool {
 }
 
 func (p *Player) Update(world *World, keys []ebiten.Key) {
+	p.frameCounter++
+	if p.frameCounter == 10 {
+		p.frame++
+		p.frameCounter = 0
+	}
+	if p.frame == 4 {
+		p.frame = 0
+	}
 	if p.first {
 		keyCopy := make([]ebiten.Key, len(keys))
 		copy(keyCopy, keys)
@@ -71,8 +107,10 @@ func (p *Player) Update(world *World, keys []ebiten.Key) {
 		switch k {
 		case ebiten.KeyLeft:
 			p.newX = p.x - runSpeed
+			p.facing = true
 		case ebiten.KeyRight:
 			p.newX = p.x + runSpeed
+			p.facing = false
 		case ebiten.KeyUp:
 			if !p.jumped {
 				p.verticalSpeed = -jumpHeight
